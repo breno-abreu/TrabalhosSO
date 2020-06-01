ec0da2a440b1954569a2b6475d514711f1d228e1
Breno Abreu <breno2601@gmail.com>
2020-06-01 19:07:49 -0300

    Projeto p07 finalizado e revisado; Correção no Makefile de outros projetos

--- ping-pong/p07/pingpong.c
+++ ping-pong/p07/pingpong.c
@@ -9,112 +9,86 @@
 #define STACKSIZE 32768
 #define QUANTUM 20
 
-task_t MainTask;                            //Tarefa referente ao contexto da função main
-task_t *CurrentTask;                        //Variável que aponta para a tarefa que está em andamento
-int contador;                               //Variável que será utilizada para inicializar o campo 'tid' da struct 'task_t'...
-                                            //...tornando possível que cada tarefa tenha um identificador diferente.
-task_t dispatcher;                          //Tarefa referente ao dispatcher
-task_t *readyQueue;                         //Fila de tarefas prontas para serem executadas
-int aging;                                  //Fator de envelhecimento
-struct sigaction action;                    //Estrutura que define o tratador de sinal
-struct itimerval timer;                     //Estrutura de inicialização do timer
-int contadorTimer;                          //Contador referente ao Quantum de cada tarefa
-unsigned int tempoAtual;                    //Conta o tempo de execução do programa em milisegundos
+task_t MainTask;                            /*Tarefa referente ao contexto da função main*/
+task_t *CurrentTask;                        /*Variável que aponta para a tarefa que está em andamento*/
+int contador;                               /*Variável que será utilizada para inicializar o campo 'tid' da struct 'task_t'...
+                                            ...tornando possível que cada tarefa tenha um identificador diferente.*/
+task_t dispatcher;                          /*Tarefa referente ao dispatcher*/
+task_t *readyQueue;                         /*Fila de tarefas prontas para serem executadas*/
+int aging;                                  /*Fator de envelhecimento*/
+struct sigaction action;                    /*Estrutura que define o tratador de sinal*/
+struct itimerval timer;                     /*Estrutura de inicialização do timer*/
+int contadorTimer;                          /*Contador referente ao Quantum de cada tarefa*/
+unsigned int tempoAtual;                    /*Conta o tempo de execução do programa em milisegundos*/
 
-//Função que será ativada quando o temporizador chegar a um determinado tempo
+/*Função que será ativada quando o temporizador chegar a um determinado tempo*/
 void tratador()
 {
-    tempoAtual++;                           //Contador de tempo de execução é atualizado a cada 1 milisegundo
-    CurrentTask->tempoProcessamento++;      //Tempo de processamento da tarefa atual é atualizado
+    /*Tempo de processamento da tarefa atual é atualizado*/
+    CurrentTask->tempoProcessamento++;
 
-    //Caso seja uma tarefa de usuário
+    /*Caso seja uma tarefa de usuário*/
     if(CurrentTask->tarefaUsuario == 1){
-        //Caso o quantum chegue a zero, seu quantum é atualizado para o valor de quantum predefinido, ...
-        //...,a tarefa é adicionada ao final da fila de prontos e o contexto é mudado para o dispatcher
+
+        /*Caso o quantum chegue a zero, seu quantum é atualizado para o valor de quantum predefinido, ...
+        ...,a tarefa é adicionada ao final da fila de prontos e o contexto é mudado para o dispatcher*/
         if(CurrentTask->contadorQuantum <= 0){
             CurrentTask->contadorQuantum = CurrentTask->quantumEstatico;
             task_yield();
         }
-        CurrentTask->contadorQuantum--;     //Decrementa o contador de quantum da tarefa atual
+        /*Decrementa o contador de quantum da tarefa atual*/
+        CurrentTask->contadorQuantum--;
     }
-    
+    /*Contador de tempo de execução é atualizado a cada 1 milisegundo*/
+    tempoAtual++;
 }
 
-//Retorna a próxima tarefa a ser executada
+/*Retorna a próxima tarefa a ser executada*/
 task_t* scheduler()
 {
-    /*task_t* aux = readyQueue;
-    task_t* tarefa = NULL;
-    int minPrio = 21;                       //Variável que recebe o valor de prioridade da tarefa com maior prioridade
-    int minIden = 10;                       //Variável que recebe o valor identificador da tarefa com maior prioridade
-    int tamanhoFila = queue_size((queue_t*) readyQueue);//Recebe o tamanho da fila de prontos
-
-    //Percorre a lista de prontos
-    for(int i = 0; i < tamanhoFila; i++){
-        //Se o valor da prioridade dinâmica for menor que o valor de 'minPrio', 'minPrio' é atualizada assim como a tarefa que será retornada.
-        //Além disso, caso duas tarefas tenham o mesmo valor de prioridade dinâmica, o fator de desempate é qual delas possui o menor valor identificador 'tid'
-        if(aux->prioridadeDinamica < minPrio || (aux->prioridadeDinamica == minPrio && aux->tid < minIden)){
-            minPrio = aux->prioridadeDinamica;
-            minIden = aux->tid;
-            tarefa = aux;
-        }
-        aux = aux->next;
-    }
-
-    //Percorre a fila de prontos novamente
-    for(int i = 0; i < tamanhoFila; i++){
-        //Caso a tarefa não seja a escolhida para ser retornada, ou seja, não seja a de maior prioridade, ...
-        //...ela é envelhecida somando sua prioridade dinâmica com o valor predefinido 'aging'
-        if(aux != tarefa){
-            aux->prioridadeDinamica += aging;
-            if(aux->prioridadeDinamica < -20)
-                aux->prioridadeDinamica = -20;
-        }
-        //Caso a tarefa seja a escolhida, ou seja, tem maior prioridade, seu valor de prioridade dinâmica é atualizada recebendo seu valor de prioridade estática
-        else
-            aux->prioridadeDinamica = aux->prioridadeEstatica;
-
-        aux = aux->next;
-    }
-    return tarefa;                          //Retorna a tarefa de maior prioridade*/
-
-    return readyQueue;                      //Retorna o primeiro elemento da fila de prontos
+    return readyQueue;                      /*Retorna o primeiro elemento da fila de prontas*/
 }
 
-//Função recebida pelo dispatcher, irá executar as tarefas que estão na fila, enquanto houverem elementos
+/*Função recebida pelo dispatcher, irá executar as tarefas que estão na fila, enquanto houverem elementos*/
 void dispatcher_body()
 {
     task_t * next;
-    //Enquanto há elementos na fila de prontos continua executando as tarefas
+
+    /*Enquanto há elementos na fila de prontos continua executando as tarefas*/
     while(queue_size((queue_t*)readyQueue) > 0){
-        next = scheduler();                 //next recebe a próxima tarefa a ser executada
-        next->ativacoes++;                  //Atualiza a quantidade de ativações da tarefa a ser executada
+        next = scheduler();                 /*next recebe a próxima tarefa a ser executada*/
+        next->ativacoes++;                  /*Atualiza a quantidade de ativações da tarefa a ser executada*/
         if(next){
             task_switch(next);
         }
     }
-    task_exit(0);                           //Após executar todas as tarefas, retorna para o contexto da função main ou para o dispatcher
+    /*Após executar todas as tarefas, retorna para o contexto da função main ou para o dispatcher*/
+    task_exit(0);
 }
 
-//Inicializa algumas variáveis e desativa o buffer da saída padrão
 void pingpong_init()
 {
+    /*desativa o buffer da saida padrao (stdout), usado pela função printf*/
+    setvbuf (stdout, 0, _IONBF, 0);
 
-    setvbuf (stdout, 0, _IONBF, 0);         //desativa o buffer da saida padrao (stdout), usado pela função printf
     readyQueue = NULL;
     contador = -1;
 
-    task_create(&MainTask, NULL, NULL);     //Cria a tarefa main, que fará referência a função principal do programa
-    CurrentTask = &MainTask;                //Inicialmente a tarefa atual será a relacionada a função main
+    /*Cria a tarefa main, que fará referência a função principal do programa*/
+    task_create(&MainTask, NULL, NULL);
 
-    task_create(&dispatcher, dispatcher_body, NULL);    //Cria a tarefa dispatcher
+    /*Inicialmente a tarefa atual será a função main*/
+    CurrentTask = &MainTask;
+
+    /*Cria a tarefa dispatcher*/
+    task_create(&dispatcher, dispatcher_body, NULL);
     dispatcher.tarefaUsuario = 0;
 
     aging = -1;
     contadorTimer = QUANTUM;
     tempoAtual = -1;
 
-    //Registra a ação para o sinal de timer SIGALRM
+    /*Registra a ação para o sinal de timer SIGALRM*/
     action.sa_handler = tratador;
     sigemptyset (&action.sa_mask);
     action.sa_flags = 0;
@@ -122,33 +96,35 @@ void pingpong_init()
         perror ("Erro em sigaction: ");
         exit (1);
     }
-    //Ajusta valores  do temporizador, que irá gerar um pulso a cada 1 milisegundo
+
+    /*Ajusta valores  do temporizador, que irá gerar um pulso a cada 1 milisegundo*/
     timer.it_value.tv_usec = 1;             // primeiro disparo, em micro-segundos
     timer.it_value.tv_sec  = 0;             // primeiro disparo, em segundos
     timer.it_interval.tv_usec = 1000;       // disparos subsequentes, em micro-segundos
     timer.it_interval.tv_sec  = 0;          // disparos subsequentes, em segundos
 
-    //Arma o temporizador ITIMER_REAL
+    /*Arma o temporizador ITIMER_REAL*/
     if (setitimer (ITIMER_REAL, &timer, 0) < 0){
         perror ("Erro em setitimer: ");
         exit (1);
     }
 
-    task_yield();                           //Dispatcher é ativado
+    /*Dispatcher é ativado*/
+    task_yield();
 }
 
 int task_create( task_t *task,
                  void (*start_func)(void *),
                  void *arg)
 {
-
     char *stack;
     contador++;
-    //Inicializa o contexto de uma tarefa
+
+    /*Inicializa o contexto de uma tarefa*/
     if(getcontext(&task->context) < 0)
         return -1;
 
-    //Preenche os campos necessários do contexto antes de chamar a função 'makecontext()'
+    /*Preenche os campos necessários do contexto antes de chamar a função 'makecontext()'*/
     stack = malloc(STACKSIZE);
     if(stack){
         task->context.uc_stack.ss_sp = stack;
@@ -161,25 +137,36 @@ int task_create( task_t *task,
         return -1;
     }
 
-    makecontext(&task->context, (void*)start_func, 1, (char*) arg); //Relaciona a função 'start_func' com o contexto em questão
-    task->tid = contador;                   //Dá a tarefa um identificador único
+    /*Relaciona a função 'start_func' com o contexto em questão*/
+    makecontext(&task->context, (void*)start_func, 1, (char*) arg);
 
-    //Código para depuração caso DEBUG esteja definido
+    /*Dá a tarefa um identificador único*/
+    task->tid = contador;
+
     #ifdef DEBUG
     printf("task_create: criou tarefa %d\n", task->tid);
     #endif
 
-    //Inclui tarefas novas na lista de prontos, a menos que a tarefa seja o dispatcher
+    /*Inclui tarefas novas na lista de prontas, a menos que a tarefa seja o dispatcher*/
     if(task != &dispatcher)
         queue_append((queue_t**) &readyQueue, (queue_t*) task);
 
-    //Inicializa os valores de prioridade estática e dinâmica usadas no escalonamento
+    /*Inicializa os valores de prioridade estática e dinâmica usadas no escalonamento*/
     task->prioridadeEstatica = 0;
     task->prioridadeDinamica = 0;
 
-    task->tarefaUsuario = 1;                //Com valor 1 significa que a tarefa é de usuário
+    /*Com valor 1 significa que a tarefa é de usuário*/
+    task->tarefaUsuario = 1;
+
     task->quantumEstatico = QUANTUM;
-    task->contadorQuantum = task->quantumEstatico;//Inicializado o contador de quantum com o quantum predefinido
+
+    /*Inicializado o contador de quantum com o quantum predefinido*/
+    task->contadorQuantum = task->quantumEstatico;
+
+    /*Tarefa Main será inicializada com seu contador de quantum em 0, dando o contexto para a próxima tarefa imediatamente*/
+    if(task->tid == 0)
+         task->contadorQuantum = 1;
+
     task->tempoExecucao = systime();
     task->tempoProcessamento = 0;
     task->ativacoes = 0;
@@ -189,16 +176,15 @@ int task_create( task_t *task,
 
 int task_switch (task_t *task)
 {
-
-    task_t *Aux = CurrentTask;              //O ponteiro 'CurrentTask' aponta para a tarefa recebida
+    /*O ponteiro 'CurrentTask' aponta para a tarefa recebida*/
+    task_t *Aux = CurrentTask;
     CurrentTask = task;
 
-    //Código para depuração caso DEBUG esteja definido
     #ifdef DEBUG
     printf("task_switch: trocando contexto %d -> %d\n", Aux->tid, task->tid);
     #endif
 
-    //Salva o contexto da tarefa anterior e aciona o contexto da tarefa recebida
+    /*Salva o contexto da tarefa anterior e aciona o contexto da tarefa recebida*/
     if(swapcontext(&Aux->context, &task->context) < 0)
         return -1;
 
@@ -207,21 +193,22 @@ int task_switch (task_t *task)
 
 void task_exit (int exitCode)
 {
-    //Código para depuração caso DEBUG esteja definido
     #ifdef DEBUG
     printf("task_exit: tarefa %d sendo encerrada\n", CurrentTask->tid);
     #endif
 
-    //Caso a tarefa atual não seja o dispatcher, remove a tarefa atual da fila de prontos e...
-    //...troca para a tarefa dispatcher
+    /*Caso a tarefa atual não seja o dispatcher, remove a tarefa atual da fila de prontos e...
+    ...troca para a tarefa dispatcher*/
     if(CurrentTask != &dispatcher){
         queue_remove((queue_t**) &readyQueue, (queue_t*) CurrentTask);
         CurrentTask->tempoExecucao = systime() - CurrentTask->tempoExecucao;
         printf("Task: %d exit: running time %d ms, cpu time %d ms, %d activations\n",
                 CurrentTask->tid, CurrentTask->tempoExecucao, CurrentTask->tempoProcessamento, CurrentTask->ativacoes);
+        dispatcher.ativacoes++;
         task_switch(&dispatcher);
     }
-    //Caso a tarefa a ser finalizada seja o dispatcher, troca para a tarefa main
+
+    /*Caso a tarefa a ser finalizada seja o dispatcher, troca para a tarefa main*/
     else{
         dispatcher.tempoExecucao = systime() - dispatcher.tempoExecucao;
         printf("Task: %d exit: running time %d ms, cpu time %d ms, %d activations\n",
@@ -232,7 +219,7 @@ void task_exit (int exitCode)
 
 int task_id ()
 {
-    return CurrentTask->tid;                //Retorna o identificador da tarefa que está em andamento
+    return CurrentTask->tid;                /*Retorna o identificador da tarefa que está em andamento*/
 }
 
 void task_yield ()
@@ -241,26 +228,29 @@ void task_yield ()
     printf("task_yield. Tarefa atual: %d. Trocando para dispatcher\n", CurrentTask->tid);
     #endif
 
-    //Caso a tarefa atual não seja o dispatcher, exclui o elemento da lista...
-    //..e o reposiciona no final da fila
+    /*Caso a tarefa atual não seja o dispatcher, exclui o elemento da lista...
+    ..e o reposiciona no final da fila*/
     if(CurrentTask != &dispatcher){
         queue_remove((queue_t**) &readyQueue, (queue_t*) CurrentTask);
         queue_append((queue_t**) &readyQueue, (queue_t*) CurrentTask);
     }
-    dispatcher.ativacoes++;                 //Atualiza a quantidade de ativações do dispatcher
-    task_switch(&dispatcher);               //Troca para o dispatcher
+
+    dispatcher.ativacoes++;                 /*Atualiza a quantidade de ativações do dispatcher*/
+    task_switch(&dispatcher);               /*Troca para o dispatcher*/
 }
 
 void task_suspend (task_t *task, task_t **queue)
 {
     if(queue && task){
         queue_t * aux = (queue_t*) task;
-        //Nó é retirado caso ja pertença a alguma fila
+
+        /*Nó é retirado caso ja pertença a alguma fila*/
         if(aux->next != NULL && aux->prev != NULL){
             aux->prev->next = aux->next;
             aux->next->prev = aux->prev;
         }
-        //Adiciona o nó na fila de prontos
+
+        /*Adiciona o nó na fila de prontos*/
         queue_append((queue_t**) &queue, aux);
     }
 }
@@ -268,12 +258,14 @@ void task_suspend (task_t *task, task_t **queue)
 void task_resume (task_t *task)
 {
     queue_t * aux = (queue_t*) task;
-    //Nó é retirado caso ja pertença a alguma fila
+
+    /*Nó é retirado caso ja pertença a alguma fila*/
     if(aux->next != NULL && aux->prev != NULL){
         aux->prev->next = aux->next;
         aux->next->prev = aux->prev;
     }
-    //Adiciona o nó na fila de prontos
+
+    /*Adiciona o nó na fila de prontos*/
     queue_append((queue_t**) &readyQueue, aux);
 }
 
@@ -283,36 +275,40 @@ void task_setprio (task_t *task, int prio)
     printf("task_setprio. Escrevendo o valor de prioridade da tarefa: %d, para %d\n", task->tid, prio);
     #endif
 
-    //Caso o valor 'prio' recebido esteja entre -20 e 20, ou seja cumpre com os valores de prioridade padrões no estilo UNIX
+    /*Caso o valor 'prio' recebido esteja entre -20 e 20, ou seja cumpre com os valores de prioridade padrões no estilo UNIX*/
     if(prio >= -20 && prio <= 20){
-        //Caso a tarefa recebida seja nula, a tarefa atual recebe os valores de prioridade
+
+        /*Caso a tarefa recebida seja nula, a tarefa atual recebe os valores de prioridade*/
         if(!task){
             CurrentTask->prioridadeEstatica = prio;
             CurrentTask->prioridadeDinamica = prio;
         }
-        //Caso contrário, a tarefa recebida recebe os valores de prioridade
+
+        /*Caso contrário, a tarefa recebida recebe os valores de prioridade*/
         else{
             task->prioridadeEstatica = prio;
             task->prioridadeDinamica = prio;
         }
     }
-    //Se o valor de 'prio' esteja além dos limites dos valores de prioridade do padrão UNIX, mostra a seguinte mensagem de erro
+
+    /*Se o valor de 'prio' esteja além dos limites dos valores de prioridade do padrão UNIX, mostra a seguinte mensagem de erro*/
     else
         printf("ERRO: prioridade está fora do limite");
 }
 
 int task_getprio (task_t *task)
 {
-    //Caso a tarefa seja nula, retorna o valor da prioridade estática da tarefa atual
+    /*Caso a tarefa seja nula, retorna o valor da prioridade estática da tarefa atual*/
     if(!task)
         return CurrentTask->prioridadeEstatica;
-    //Caso contrário, retrona o valor da prioridade estática da tarefa recebida
+
+    /*Caso contrário, retrona o valor da prioridade estática da tarefa recebida*/
     return task->prioridadeEstatica;
 }
 
 unsigned int systime ()
 {
-    return tempoAtual;                      //Retorna o tempo atual de execução do programa
+    return tempoAtual;                      /*Retorna o tempo atual de execução do programa*/
 }
 
 
--- ping-pong/p07_CB/queue.c
+++ ping-pong/p07_CB/queue.c
@@ -1,23 +1,27 @@
 #include "queue.h"
 #include <stdio.h>
 
-//Função de inserção de um elemento 'elem' em uma lista 'queue'
+/*Função de inserção de um elemento 'elem' em uma lista 'queue'*/
 void queue_append (queue_t **queue, queue_t *elem){
-    //Caso o elemento seja nulo, imprime uma mensagem de erro e não insere na lista
+
+    /*Caso o elemento seja nulo, imprime uma mensagem de erro e não insere na lista*/
     if(!elem)
         printf("ERRO: elemento nao existe!\n");
+
     /*Caso o elemento pertença a outra fila, seus membros 'next' e 'prev' não serão nulos e estarão...
     ...apontando para outro elemento de outra lista, portanto o elemento não pode ser inserido nessa lista...
     ...Para tal é imprimido uma mensagem de erro*/
     else if(elem->next != NULL || elem->prev != NULL)
         printf("ERRO: elemento ja pertence a alguma fila!\n");
-    //Caso a lista esteja vazia, será a inserido seu primeiro elemento
+
+    /*Caso a lista esteja vazia, será a inserido seu primeiro elemento*/
     else if((*queue) == NULL){
         *queue = elem;
         (*queue)->next = elem;
         (*queue)->prev = elem;
     }
-    //Caso a lista já tenha ao menos um elemento, será inserido no final
+
+    /*Caso a lista já tenha ao menos um elemento, insere o novo elemento no final da lista*/
     else{
         queue_t *aux = (*queue)->prev;
         elem->next = (*queue);
@@ -27,24 +31,30 @@ void queue_append (queue_t **queue, queue_t *elem){
     }
 }
 
-//Função de exclusão de um elemento 'elem' de uma lista 'queue'
+/*Função de exclusão de um elemento 'elem' de uma lista 'queue'*/
 queue_t *queue_remove (queue_t **queue, queue_t *elem){
     queue_t *aux = NULL;
-    //Caso a fila esteja vazia, imprime uma mensagem de erro
+
+    /*Caso a fila esteja vazia, imprime uma mensagem de erro*/
     if(!(*queue))
         printf("ERRO: Fila esta vazia!\n");
-    //Caso o elemento recebido não exista, imprime uma mensagem de erro
+
+    /*Caso o elemento recebido não exista, imprime uma mensagem de erro*/
     else if (!elem)
         printf("ERRO: elemento nao existe!\n");
-    //Caso tudo esteja correto para exclusão
+
+    /*Caso tudo esteja correto para exclusão*/
     else{
         aux = (*queue);
-        //Caso haja apenas um elemento na lista e seja igual ao elemento
+
+        /*Caso haja apenas um elemento na lista e seja igual ao elemento*/
         if(aux == elem){
-            //Caso haja apenas um elemento na lista, ou seja, o membro 'next' aponta para ele mesmo
+
+            /*Caso haja apenas um elemento na lista, ou seja, o membro 'next' aponta para ele mesmo*/
             if(aux->next == aux)
                 (*queue) = NULL;
-            //Caso haja dois ou mais itens na lista
+
+            /*Caso haja dois ou mais itens na lista*/
             else{
                 aux->prev->next = aux->next;
                 aux->next->prev = aux->prev;
@@ -54,6 +64,7 @@ queue_t *queue_remove (queue_t **queue, queue_t *elem){
             aux->prev = NULL;
             return aux;
         }
+
         /*Percorre os demais itens da lista procurando o correto para exclusão
         Essa parte do algoritmo apenas testa todos os elementos que não sejam o primeiro da lista, e nem...
         ...se o segundo elemento for o último, ou seja, o membro 'next' aponta para o inicio da lista...
@@ -72,6 +83,7 @@ queue_t *queue_remove (queue_t **queue, queue_t *elem){
                 return aux;
             }
         }
+
         /*Caso o elemento não seja encontrado nos dois algoritmos acima, ele não pertence a esta lista.
         Se isso ocorrer, uma mensagem de erro é mostrada.*/
         printf("ERRO: elemento nao pertence a lista em questao!\n");
@@ -80,14 +92,16 @@ queue_t *queue_remove (queue_t **queue, queue_t *elem){
     return aux;
 }
 
-//Função que retorna a quantidade de itens em uma lista
+/*Função que retorna a quantidade de itens em uma lista*/
 int queue_size (queue_t *queue) {
     int cont = 0;
+
     /*Caso a lista esteja vazia o teste if será pulado, retornando zero.
     Caso contrário a lista será percorrida e um contador irá armazenar a quantidade de elementos*/
-    if(queue != NULL){
+    if(queue){
         cont++;
         queue_t *aux = queue;
+
         /*O laço while a seguir conta um item a menos, pois quando o percorrimento da lista chega...
         ...no último elemento, percebe que o membro 'next' aponta para o inicio da lista e o laço termina, sem contá-lo.
         Por isso é necessário adicionar 1 ao contador antes de iniciar o laço.*/
@@ -99,14 +113,16 @@ int queue_size (queue_t *queue) {
     return cont;
 }
 
-//Função que percorre a lista e imprime o conteudo 'id' de cada elemento, além do 'id' do elemento anterior e do posterior
+/*Função que percorre a lista e imprime o conteudo 'id' de cada elemento, além do 'id' do elemento anterior e do posterior*/
 void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
     printf("%s: [", name);
-    //Caso a lista exista.
+
+    /*Caso a lista exista.*/
     if(queue){
         queue_t *aux = queue;
         print_elem(aux);
-        //Percorre a lista e imprime o conteudo de cada elemento
+
+        /*Percorre a lista e imprime o conteudo de cada elemento*/
         while(aux->next != queue){
             printf(" ");
             aux = aux->next;
